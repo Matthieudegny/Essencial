0f84994304bb0d67c20c642763d5ff9089c9fe18
ManyDantes <matthieu.degny.dev@gmail.com>
2022-06-12 21:04:29 +0200

    sidebar

--- my-app/src/components/Sidebar.js
+++ my-app/src/components/Sidebar.js
@@ -1,30 +1,50 @@
 // == Imports
 import React from "react";
 import { NavLink } from "react-router-dom";
+import '../styles/sideBar.scss'
 
 const Sidebar = () => {
 
     return (
     <div className="sidebar">
-        <ul>
-            <li>
-              <NavLink to="/tutos" > 
-              <span>Tutoriel</span>
-              </NavLink>
-            </li>
+        {/* <div classNameName="sidebar-link">
+          <NavLink to="/tutos" > 
+            <span>Tutoriel</span>
+          </NavLink>
+        </div>
+        <div classNameName="sidebar-link">
+          <NavLink to="/amis" > 
+            <span>Amis</span>
+          </NavLink>
+        </div>
+        <div classNameName="sidebar-link">
+          <NavLink to="/eco-village-vue" > 
+            <span>Les Ecovillages</span>
+          </NavLink>
+        </div>  */}
 
-            <li>
-              <NavLink to="/amis" > 
-              <span>Amis</span>
-              </NavLink>
-            </li>
-
-            <li>
-              <NavLink to="/eco-village-vue" > 
-              <span>Les Ecovillages</span>
-            </NavLink>
-            </li>
-        </ul>
+       {/* {Bulma's version } */}
+      <aside className="menu">
+  
+        <ul className="menu-list test">
+          <li>
+          <NavLink to="/tutos"  > 
+            <span>Tutoriel</span>
+          </NavLink>
+          </li>
+          <li>
+          <NavLink to="/amis"  > 
+            <span>Amis</span>
+          </NavLink>
+          </li>
+          <li>
+          <NavLink to="/eco-village-vue"  > 
+            <span>Les Ecovillages</span>
+          </NavLink>
+          </li>
+        </ul>      
+      </aside>  
+     
     </div>    
     );
 };
