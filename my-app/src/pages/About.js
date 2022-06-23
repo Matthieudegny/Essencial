c0ed66f6badf6d6264630d2b89deb5c004929ba5
GOD-WITH-US <e.cronenberger@gmail.com>
2022-06-23 09:10:40 +0200

    about contact ok

--- my-app/src/pages/About.js
+++ my-app/src/pages/About.js
@@ -5,11 +5,36 @@ import React from "react";
 const error404 = () => {
     return (
         <div className="home" >
-     
-           
-             Ici je suis sur ma page About
-          
 
+            Essencial a été créé en mai 2022 par Stephanie , Matthieu , Arthur et Emmanuel avec le désir d'offrir  <br /> une plateforme d'échange
+            au personnes souhaitant se reconnecter avec la nature et l'humain .
+
+            <br />
+            <br />
+            Aujourd’hui notre planète s’essouffle un peu plus chaque jour et la remise en question de notre mode de vie est devenue une évidence.
+            <br />
+            Redonnez du sens à notre life style et reconnectez- vous vraiment avec les autres et avec la nature.
+            <br />
+            <br />
+            Ensemble partageons et grandissons pour construire dès maintenant un monde meilleur Bref revenons vers l’essentiel !  
+            <br />
+            <br/>
+
+            <br/>
+            <br/>
+            <br/>
+           
+            <br />
+            <br/>
+            le premier réseau social d'entraide visant à un retour vers une plus grande autonomie et le respect de notre planète.
+            <br />
+            <br/>
+
+            <br/>
+            <br/>
+            <br/>
+
+            Les 4 fantastiques ont mis leurs compétences et tout leur amour pour vous présenter aujourd'hui Essencial
         </div>
     );
 };
