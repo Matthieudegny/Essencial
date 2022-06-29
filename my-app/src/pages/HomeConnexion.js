53c751d5aa4b13460f172f42611e9340f438e638
ManyDantes <matthieu.degny.dev@gmail.com>
2022-06-29 09:48:53 +0200

    last commit before merge

--- my-app/src/pages/HomeConnexion.js
+++ my-app/src/pages/HomeConnexion.js
@@ -12,19 +12,19 @@ const HomeConnexion = () => {
            
            <div className="homeConnexion-container-form">
 
-            <div className="homeConnexion-form">
-            <AppHeader />
-            </div>
+                <div className="homeConnexion-form">
+                <AppHeader />
+                </div>
 
-            <div className="homeConnexion-visiteur">
-                <h2>Accès Simple Visiteur</h2>
-                <p> Vous n'aurez pas la possibilité de partage sur notre site, c'est bien dommage </p>
-                    <button className="homeConnexion-visiteur-button" >
-                        <NavLink to="/accueil" > 
-                            Visiter
-                        </NavLink>
-                    </button>
-            </div>
+                <div className="homeConnexion-visiteur">
+                    <h2>Accès Simple Visiteur</h2>
+                    <p> Vous n'aurez pas la possibilité de partage sur notre site, c'est bien dommage </p>
+                        <button className="homeConnexion-visiteur-button" >
+                            <NavLink to="/accueil" > 
+                                Visiter
+                            </NavLink>
+                        </button>
+                </div>
 
            </div>
 
