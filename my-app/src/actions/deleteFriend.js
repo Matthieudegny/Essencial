0aa18244bceea5d1fdf9e29f1e9ba02195d67818
Matthieu Degny <104087150+Matthieudegny@users.noreply.github.com>
2022-07-05 17:37:18 +0200

    Delete my-app directory
    remove my app

--- my-app/src/actions/deleteFriend.js
+++
@@ -1,9 +0,0 @@
-export const DELETE_FRIEND = 'DELETE_FRIEND'
-
-export const actionDeleteFriend = (id,idUser) => {
-  //console.log(id);
-  return{
-    type: DELETE_FRIEND,
-    payload: {id, idUser}
-  }
-};
--- my-app/src/selectors/user.js
+++
@@ -1,7 +0,0 @@
-export function findUser(users, userId) {
-    const user = users.find((testedUser) => {
-      return testedUser.id === userId;
-    });
-    return user;
-  }
-  
